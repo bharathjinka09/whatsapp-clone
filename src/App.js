386f1ba1bbdeb87462e81f8ca16c2e407f83ca82
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-19 18:04:16 +0530

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div className="App">
-     <h1>Whatsapp Clone</h1> 
+    <div className="app">
+      {/* <h1>Whatsapp Clone</h1> */}
+      <div className="app__body">
+          {/* sidebar */}
+          <Sidebar />
+          {/* chat */}
+      </div>
     </div>
   );
 }
