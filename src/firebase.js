1cc0131921f032744a3a7fbbe1666e214cd4f39c
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-19 16:17:55 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDWgUyU3ilm9sdYz82yUnYjTfy8kPZG_Is",
+  authDomain: "whatsapp-clone-c7141.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-c7141.firebaseio.com",
+  projectId: "whatsapp-clone-c7141",
+  storageBucket: "whatsapp-clone-c7141.appspot.com",
+  messagingSenderId: "452362295210",
+  appId: "1:452362295210:web:29c1e56f708d779f0dfdd3",
+  measurementId: "G-WW8MPHN2VP"
+};
+
+export default firebaseConfig
