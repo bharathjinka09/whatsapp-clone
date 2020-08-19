2b3c1444bb247cc61964d306659a2ec819593dc9
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-19 18:08:38 +0530

    Updates

---
+++ precache-manifest.bc0f07f71f237af6b772bde070945cea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b17e40b22e4c809433a46e4762e590e",
+    "url": "/whatsapp-clone/index.html"
+  },
+  {
+    "revision": "4ec2b82625d067ab83ea",
+    "url": "/whatsapp-clone/static/css/main.9a71bbae.chunk.css"
+  },
+  {
+    "revision": "aae3a63d30adc6cefc20",
+    "url": "/whatsapp-clone/static/js/2.8af7720f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/whatsapp-clone/static/js/2.8af7720f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ec2b82625d067ab83ea",
+    "url": "/whatsapp-clone/static/js/main.355e79a8.chunk.js"
+  },
+  {
+    "revision": "8d0a8cdab6927a1f4f08",
+    "url": "/whatsapp-clone/static/js/runtime-main.a04c35f0.js"
+  }
+]);
