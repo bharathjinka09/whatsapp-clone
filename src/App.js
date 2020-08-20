aa818910109b2e06785d379265278696ba3ce7ff
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-20 13:17:24 +0530

    Added chat header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
 import Sidebar from "./Sidebar";
+import Chat from "./Chat";
 
 function App() {
   return (
     <div className="app">
       {/* <h1>Whatsapp Clone</h1> */}
       <div className="app__body">
-          {/* sidebar */}
-          <Sidebar />
-          {/* chat */}
+        <Sidebar />
+        <Chat />
       </div>
     </div>
   );
---
+++ src/as.js
@@ -0,0 +1,6 @@
+let details = {
+  name: "bharath",
+  age: 22,
+};
+
+console.log(details.name);
